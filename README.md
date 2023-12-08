9e71f71e26cb32070e577e5b1d8acc2d26331c9b
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2023-12-08 17:38:07 +0530

    Completed hooks-basics

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Assignments
+
+| Title       | Folder Name    | Live Link                                         |
+| ----------- | -------------- | ------------------------------------------------- |
+| Hooks Basic | `hooks-basics` | https://hooks-basics-assignment.netlify.app |
---
+++ hooks-basics/src/components/Like.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Like() {
+  const [likeCount, setLikeCount] = useState(0);
+
+  return (
+    <div>
+      <p style={{ fontWeight: "700" }}>{likeCount}</p>
+      <button onClick={() => setLikeCount((prev) => prev + 1)}>Like</button>
+    </div>
+  );
+}
+
+export default Like;
